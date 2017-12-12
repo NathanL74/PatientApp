71016db900a227be09af484718a4d82deba06ce5
la.xavier <xavierla74@hotmail.fr>
2017-12-12 11:11:30 +0100

    Création de detail activity et création d'un spinner

--- app/src/main/java/com/example/lestiennathan/patientapp/DetailActivity.java
+++ app/src/main/java/com/example/lestiennathan/patientapp/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.lestiennathan.patientapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -11,22 +13,28 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Button boutonSuppr = (Button) findViewById(R.id.ButtonSupprimer);
+
         Patient monPatient = (Patient) getIntent().getSerializableExtra("Patient : ");
 
         TextView textDetail;
         TextView TextViewNomDetail = (TextView) findViewById(R.id.TextViewNomDetail);
         TextViewNomDetail.setText(monPatient.toString());
 
-        TextView textDetailEspece;
-        TextView textViewEspeceDetail = (TextView) findViewById(R.id.textViewEspeceDetail);
-        textViewEspeceDetail.setText(monPatient.toString());
+        TextView textDetailPrenom;
+        TextView textViewPrenomDetail = (TextView) findViewById(R.id.TextViewPrenomDetail);
+        textViewPrenomDetail.setText(monPatient.toString());
 
-        TextView textDetailAge;
-        TextView textViewAgeDetail = (TextView) findViewById(R.id.textViewAgeDetail);
-        textViewAgeDetail.setText(monPatient.toString());
+        TextView textAge;
+        TextView TextViewAgeDetail = (TextView) findViewById(R.id.TextViewAgeDetail);
+        TextViewAgeDetail.setText(monPatient.toString());
 
-        TextView textDetailStatut;
-        TextView textViewStatutDetail = (TextView) findViewById(R.id.textViewStatutDetail);
-        textViewStatutDetail.setText(monPatient.toString());
+        TextView textDetailChambre;
+        TextView TextViewNumChambreDetail = (TextView) findViewById(R.id.TextViewNumChambreDetail);
+        TextViewNumChambreDetail.setText(monPatient.toString());
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Motifs, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
     }
 }
